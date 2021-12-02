46037673877e64810ab40deabea154240fc2f68e
Andika Rangga S <andika18andro@gmail.com>
2021-12-02 20:34:34 +0700

    E-Service app rename

--- resources/views/dashboard/homeBase.blade.php
+++ resources/views/dashboard/homeBase.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>E-Government</title>
+    <title>E-Service</title>
     <link rel="manifest" href="assets/favicon/manifest.json">
     <!-- Icons-->
     <link href="{{ asset('css/clientlib-base.min.css') }}" rel="stylesheet">
