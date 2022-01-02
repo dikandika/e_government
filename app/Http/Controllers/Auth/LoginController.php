9b6b7c5360fe61ffea2108694589e3f9f0e96017
Andika Rangga S <andika18andro@gmail.com>
2022-01-02 20:02:07 +0700

    redirect to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/skck';
+    protected $redirectTo = '/report';
 
     /**
      * Create a new controller instance.
